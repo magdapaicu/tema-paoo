4ceba64e8390c6912388b616a66051d75a93f40c
magdapaicu <paicumagda@yahoo.com>
2023-10-27 16:52:09 +0300

    create constructor for class

--- tema1.cpp
+++ tema1.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 #include <string>
 
-class Student{
-    private :
-    std::string firstname,lastname;
-    int age;
-    int numberGrades;
-    double* grades;
-    double averageGrade;
+class Student {
+private:
+  std::string firstname, lastname;
+  int age;
+  int numberGrades;
+  double *grades;
+  double averageGrade;
+
+public:
+  Student(std::string firstname, std::string lastname, int age,
+          int numberGrades, double *grades, double averageGrade)
+      : firstname(firstname), lastname(lastname), age(age),
+        numberGrades(numberGrades), averageGrade(0.0) {
+
+    grades = new double[numberGrades];
+
+    if (numberGrades > 0) {
+      for (int i = 0; i < numberGrades; i++) {
+        grades[i] = grades[i];
+      }
+    } else {
+      grades = nullptr;
+    }
+  }
 };
 
-int main(){
-
-}
+int main() {}
